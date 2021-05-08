85a4120f939537e83fab1e9409dbefb89f53c9a1
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-08 01:25:09 +0000

    add invite command

---
+++ src/commands/general/invite.ts
@@ -0,0 +1,28 @@
+import emojis from "../../constants/emojis.ts";
+import translate from "../../languages/translate.ts";
+import { Command } from "../mod.ts";
+
+const command: Command = {
+  global: true,
+  execute: function (payload) {
+    return {
+      content: [
+        `${emojis.coin} **${
+          translate(
+            payload.guildId!,
+            "strings:INVITE_BOT",
+          )
+        }:** <https://discordapp.com/oauth2/authorize?client_id=${botID}&scope=bot+applications.commands&permissions=2111302911>`,
+        "",
+        `${emojis.bot} **${
+          translate(
+            payload.guildId!,
+            "strings:NEED_SUPPORT",
+          )
+        }:** discord.gg/J4NqJ72`,
+      ].join("\n"),
+    };
+  },
+};
+
+export default command;
---
+++ src/constants/emojis.ts
@@ -0,0 +1,6 @@
+const emojis = {
+  bot: "<:g4m3rblue:443416157597466636>",
+  coin: "<:g4m3rwhite:443416083874054145>",
+};
+
+export default emojis;
