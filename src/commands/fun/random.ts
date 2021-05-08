b6c86762b2b59efa1e9298921dc6213c7b2c134e
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-05-08 22:12:53 +0000

    test implementation for random

--- src/commands/fun/random.ts
+++ src/commands/fun/random.ts
@@ -1,6 +1,9 @@
-import { ApplicationCommandOptionTypes } from "../../../deps.ts";
+import {
+  ApplicationCommandOptionTypes,
+} from "../../../deps.ts";
 import translate from "../../languages/translate.ts";
-// import { chooseRandom } from "../../utils/helpers.ts";
+import { Embed } from "../../utils/Embed.ts";
+import { chooseRandom } from "../../utils/helpers.ts";
 import { Command } from "../mod.ts";
 
 const command: Command = {
@@ -50,17 +53,44 @@ const command: Command = {
   ],
   execute: function (payload) {
     console.log("options, random", payload.data?.options);
-    // const subcommand = payload.data?.options?.[0];
-    // if (!subcommand) {
-    return {
-      content: translate(payload.guildId!, "BROKE_DISCORD"),
-      embeds: [{ image: { url: "https://i.imgur.com/pEJNhgG.gif" } }],
-    };
-    // }
+    const subcommand = payload.data?.options?.[0];
+    if (!subcommand) {
+      return {
+        content: translate(payload.guildId!, "BROKE_DISCORD"),
+        embeds: [{ image: { url: "https://i.imgur.com/pEJNhgG.gif" } }],
+      };
+    }
 
-    // switch(subcommand.value) {
-    //   case "sub"
-    // }
+    switch (subcommand.name) {
+      case "number": {
+        const minimum =
+          subcommand.options?.find((o) => o.name === "minimum")?.value || 0;
+        const maximum =
+          subcommand.options?.find((o) => o.name === "maximum")?.value || 100;
+
+        return {
+          content: Math.floor(Math.random() * (maximum - minimum) + minimum)
+            .toLocaleString("en-US"),
+        };
+      }
+      case "advice":
+        return {
+          content: chooseRandom(
+            translate(payload.guildId!, "RANDOM_ADVICE_QUOTES").split("\n"),
+          ),
+        };
+      case "8ball":
+        return {
+          embeds: [
+            new Embed().setDescription(subcommand.options?.[0].value).addField(
+              translate(payload.guildId!, "ANSWER"),
+              chooseRandom(
+                translate(payload.guildId!, "RANDOM_8BALL_QUOTES").split("\n"),
+              ),
+            ),
+          ],
+        };
+    }
   },
 };
 
